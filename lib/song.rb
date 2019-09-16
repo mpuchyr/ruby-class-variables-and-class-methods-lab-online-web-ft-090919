7336b1bab4fa828b8766c74b8a63574b3937edfd
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-16 19:21:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,21 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@genres = []
+  @@artists = []
   
-  def initialize
+  def initialize(genre)
     @@count += 1
+    @@genre << @genre
+    @@artists << @artist
+  end
+  
+  def count
+    @@count
+  end
+  
+  def artists
+    @@artists.uniq
   end
 
 end
